b157cfa5fa88210922fca4e2e1e298ed5abf087e
cquarles14 <catlynne.quarles@sinclair.edu>
2022-05-02 23:48:31 -0400

    First commit. Function programmed to validate char. Has not been tested yet.

--- Function_Templates_Part_A/main.cpp
+++ Function_Templates_Part_A/main.cpp
@@ -1,14 +1,56 @@
-//
-//  main.cpp
-//  Function_Templates_Part_A
-//
-//  Created by Catlynne Quarles on 5/2/22.
-//
+// Catlynne Quarles
+// CIS 1202 R02
+// May 3, 2022
+// Function Templates Part A
 
 #include <iostream>
+#include <algorithm>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+
+
+char character(char start, int offset)
+{
+    char newValue;
+    string invalidRangeException = "Error: not a letter.";
+    char invalidCharacterException [] = {"Error: start must be greater that A and less than Z."};
+    {
+          try
+            {
+                cout << "Start: " << start << endl;
+                cout << "Offset: " << offset << endl;
+                
+                if (!isalpha(start))
+                {
+                    throw invalidRangeException;
+                }
+                else if (start <= 'a' || start <= 'A')
+                {
+                    throw invalidCharacterException;
+                }
+                else if (start >= 'z' || start <= 'Z')
+                {
+                    throw invalidCharacterException;
+                }
+                
+                newValue = start + offset;
+            
+                
+            }
+            catch(string invalidRangeException)
+            {
+                cout << invalidRangeException << endl;
+            }
+            catch(char invalidCharacterException[])
+            {
+                cout << invalidRangeException << endl;
+            }
+           
+        
+    }
+    return newValue;
+}
+int main()
+{
+   
 }
